cdf6ffc4f474f32185491064feaa9f96029bf820
duahaubeos <anhngochaa@gmail.com>
2025-03-18 13:24:23 +0700

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,42 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile0 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101030202020102020202020202020201010102010201020202020201010101010101010102010101010102020202010201010201020102020202020101010102010102010201020101010101010101020101020102010201010101020202020201010201020202020201010201010101010102010101010102010102010101020101020101010101020101020101010201010201020202020202020202010102010102010201010101020101020101020101020102020201010201010202020201010101020101010102010101010102010101010202020202020201020202020301010101010101010101010101010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.swamp.swampTile16], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "tile0":return tile0;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
